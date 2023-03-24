6efc93c61f8e960e2b979644d1c37e12d0d0dfb2
mettashalini89@gmail.com <CatMatDog@123>
2023-03-24 13:09:46 -0600

    app config using aws

--- userdata.sh
+++ userdata.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aansible-pull -i localhost, -U https://github.com/mettashalini89/roboshop-ansible roboshop.yml -e role_name=${component} -e env=${env}  >/opt/ansible.log
+ansible-pull -i localhost, -U https://github.com/raghudevopsb71/roboshop-ansible roboshop.yml -e role_name=${component} -e env=${env} >/opt/ansible.log

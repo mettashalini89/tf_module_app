3f08d134c026f34e5422989530b5a349a997c22a
mettashalini89@gmail.com <CatMatDog@123>
2023-03-23 20:42:16 -0600

    app config using aws

--- main.tf
+++ main.tf
@@ -18,3 +18,15 @@ resource "aws_launch_template" "main" {
     )
   }
 }
+
+resource "aws_autoscaling_group" "main" {
+  desired_capacity   = var.desired_capacity
+  max_size           = var.max_size
+  min_size           = var.min_size
+  vpc_zone_identifier = var.subnets
+
+  launch_template {
+    id      = aws_launch_template.main.id
+    version = "$Latest"
+  }
+}

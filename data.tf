b01da9c511a423cdc98105134295324d1ce5f9dc
mettashalini89@gmail.com <CatMatDog@123>
2023-03-31 11:20:42 -0600

    prod env

--- data.tf
+++ data.tf
@@ -1,11 +1,10 @@
-data "aws_ami" "ami" {
+data "aws_caller_identity" "account" {}
 
+data "aws_ami" "ami" {
   most_recent      = true
   name_regex       = "devops-practice-with-ansible"
-  owners           = ["self"]
+  owners           = [data.aws_caller_identity.account.account_id]
 }
-
-data "aws_caller_identity" "account" {}
 
 data "aws_route53_zone" "domain" {
   name = var.dns_domain
